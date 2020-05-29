5cee63db69375888e3e462467242f642d24980e5
meashishpokhrel <meashishpokhrel@gmail.com>
2020-05-29 13:39:51 +0545

    Omnifood Website

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+ $(document).ready(function(){
+
+    $('h1').click(function(){
+        $(this).css('background-color','#ff0000');
+    })
+
+ });
+
+ $(document).ready(function(){
+
+    $('h2').click(fucntion(){
+        $(this).css('color','#000000');
+
+    })
+
+ });
